55cacccf702d63d98de3c534afca733cb1d516cf
Fabio <fabio.leforte@gmail.com>
2019-05-04 16:14:18 -0300

    Criação do serviço e componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
+
+import { AppComponent } from "./app.component";
+import { PesquisaComponent } from './shared/pesquisa-bar/pesquisa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PesquisaComponent,
   ],
-  imports: [
-    BrowserModule
+  imports: [BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
